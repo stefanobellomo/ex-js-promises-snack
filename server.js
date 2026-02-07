bc8aff66bb10be95eaab1117b98353faa3703231
stefanobellomo <stefanobellomo1998@gmail.com>
2026-02-07 11:38:45 +0100

    bonus snack 1

--- server.js
+++ server.js
@@ -14,3 +14,27 @@ getPostTitle(1)
     .then(obj => console.log(obj))
     .catch(error => console.error(error));
 
+// SNACK 1 BONUS
+
+function getPost(id) {
+    const promessa = new Promise((resolve, reject) => {
+        fetch(`https://dummyjson.com/posts/${id}`)
+            .then(response => response.json())
+            .then(post => {
+                fetch(`https://dummyjson.com/users/${post.userId}`)
+                    .then(response => response.json())
+                    .then(user => {
+                        post.user = user
+                        resolve(post)
+                    })
+                    .catch(reject)
+            })
+            .catch(reject)
+    })
+    return promessa
+}
+
+getPost(1)
+    .then(result => console.log(result))
+    .catch(error => console.error(error));
+
